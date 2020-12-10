18810cd416ba604939cb0bd38dabcb0fb6cf8929
yashika0810 <khatri.yashika48@gmail.com>
2020-12-10 08:51:54 -0500

    initial

---
+++ backend/backend/api/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewset
+
+
+router=routers.DefaultRouter()
+router.register('users', UserViewset)
+
+urlpatterns = [
+
+    path('', include(router.urls)),
+]
